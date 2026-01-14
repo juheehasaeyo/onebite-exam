da66ccd800a165027d3a42e50aeb411621bbf2e9
조주희 <carla_cho@naver.com>
2026-01-15 00:27:39 +0900

    ~4.10 낙관적 업데이트

---
+++ src/hooks/mutations/use-update-todo-mutation.ts
@@ -0,0 +1,45 @@
+import { updateTodo } from "@/api/update-todo";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useUpdateTodoMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateTodo,
+    onMutate: async (updatedTodo) => {
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
+
+      const prevTodos = queryClient.getQueryData<Todo[]>(QUERY_KEYS.todo.list);
+
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [];
+        return prevTodos.map((prevTodos) =>
+          prevTodos.id === updatedTodo.id
+            ? { ...prevTodos, ...updatedTodo }
+            : prevTodos,
+        );
+      });
+
+      return {
+        prevTodos,
+      };
+    },
+    onError: (error, variable, context) => {
+      if (context && context.prevTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.prevTodos,
+        );
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.todo.list,
+      });
+    },
+  });
+}
